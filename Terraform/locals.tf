94c103f29ca4a366943b2b09f654daa20edb0aaa
Victor Iliya <VictorIliya15@gmail.com>
2024-12-07 12:43:34 +0100

    Created Terraform Project

---
+++ Terraform/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  bucket_name = "ids-project-terraform-state"
+  table_name  = "ids-terraform-lock"
+}
---
+++ Terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.9.4"
+  backend "s3" {
+    bucket         = "ids-project-terraform-state"
+    key            = "tf-state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "ids-terraform-lock"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.50.0"
+    }
+  }
+}
+
+module "tf-state" {
+  source      = "./modules/tf-state"
+  bucket_name = local.bucket_name
+  table_name  = local.table_name
+}
