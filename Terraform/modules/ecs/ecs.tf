dcf40472a339ee2b45573a0a646c8376a9c5dd00
Victor Iliya <VictorIliya15@gmail.com>
2024-12-07 19:09:12 +0100

    fmt

--- Terraform/modules/ecs/ecs.tf
+++ Terraform/modules/ecs/ecs.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "ids-task-definitions" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn    = aws_iam_role.ecs_task_execution_role.arn  # Reference the execution role
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn # Reference the execution role
 
   container_definitions = jsonencode([
     {
@@ -20,7 +20,7 @@ resource "aws_ecs_task_definition" "ids-task-definitions" {
         {
           containerPort = 8080
           hostPort      = 8080
-          protocol= "tcp"
+          protocol      = "tcp"
         }
       ]
     }
@@ -28,16 +28,16 @@ resource "aws_ecs_task_definition" "ids-task-definitions" {
 }
 
 resource "aws_ecs_service" "ids-service" {
-  name            = "ids-service"
-  cluster         = aws_ecs_cluster.ids-cluster.id
-  task_definition = aws_ecs_task_definition.ids-task-definitions.arn
-  desired_count   = 1
-  health_check_grace_period_seconds = 60  # Set grace period to 60 seconds
-  launch_type     = "FARGATE"
+  name                              = "ids-service"
+  cluster                           = aws_ecs_cluster.ids-cluster.id
+  task_definition                   = aws_ecs_task_definition.ids-task-definitions.arn
+  desired_count                     = 1
+  health_check_grace_period_seconds = 60 # Set grace period to 60 seconds
+  launch_type                       = "FARGATE"
 
   network_configuration {
-    subnets          = var.subnets 
-    security_groups  = [aws_security_group.ecs_sg.id]                 # Replace with your security group ID                                        # Change based on your setup
+    subnets         = var.subnets
+    security_groups = [aws_security_group.ecs_sg.id] # Replace with your security group ID                                        # Change based on your setup
   }
   load_balancer {
     target_group_arn = var.alb_tg
@@ -50,16 +50,16 @@ resource "aws_ecs_service" "ids-service" {
 
 # Define IAM Role for ECS Service
 resource "aws_iam_role" "ecs_service_role" {
-  name               = var.role_name
+  name = var.role_name
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow"
+        Effect = "Allow"
         Principal = {
           Service = "ecs.amazonaws.com"
         }
-        Action   = "sts:AssumeRole"
+        Action = "sts:AssumeRole"
       }
     ]
   })
@@ -74,8 +74,8 @@ resource "aws_iam_role_policy" "ecs_service_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "logs:CreateLogStream",
           "logs:PutLogEvents",
           "logs:DescribeLogStreams"
@@ -88,13 +88,13 @@ resource "aws_iam_role_policy" "ecs_service_policy" {
         Resource = "*"
       },
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "ecr:GetAuthorizationToken",
           "ecr:BatchCheckLayerAvailability",
           "ecr:GetDownloadUrlForLayer"
         ]
-        Resource = "${var.ecr_arn}"  # Correctly reference the full ECR URI
+        Resource = "${var.ecr_arn}" # Correctly reference the full ECR URI
       },
       {
         Effect   = "Allow"
@@ -107,16 +107,16 @@ resource "aws_iam_role_policy" "ecs_service_policy" {
 
 # Define the execution role for ECS Task Definition
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecs-task-execution-role"
+  name = "ecs-task-execution-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow"
+        Effect = "Allow"
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
-        Action   = "sts:AssumeRole"
+        Action = "sts:AssumeRole"
       }
     ]
   })
