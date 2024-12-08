eacaaeeaef9748a2090429117abd623f1399255b
Victor Iliya <VictorIliya15@gmail.com>
2024-12-08 05:50:20 +0100

    Separated the S3 and DynamoDB backend from the main infrastructure

--- Terraform/backend/locals.tf
+++ Terraform/backend/locals.tf
@@ -1,8 +1,4 @@
 locals {
   bucket_name     = "ids-project-terraform-state"
   table_name      = "ids-terraform-lock"
-  ecr_repo_name   = "ids-repo"
-  ids_ecs_cluster = "ids-cluster"
-  container_name  = "ids-container"
-  role_name       = "ecs-service-role"
 }
